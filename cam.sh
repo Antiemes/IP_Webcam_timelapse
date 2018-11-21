fabf6224b08c91b7dbb304dc7e1d6018b0af87c0
Antiemes <antiemes@gmail.com>
2018-11-21 14:57:51 +0100

    Timeout added

--- cam.sh
+++ cam.sh
@@ -5,6 +5,6 @@ while true
     x=$(date +%s)
     d=$((x-x%10+10))
     sleep $((d-x))
-    wget "http://192.168.1.243:8080/photoaf.jpg" -O photo_$d.jpg
+    wget "http://192.168.1.243:8080/photoaf.jpg" --timeout=3 -O photo_$d.jpg
     echo $d
   done
